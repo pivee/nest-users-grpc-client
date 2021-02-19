3e3e48c2eb19d3d5e46cbb3d756c45e49e6c46db
Pivithuru Jayakody <piveedj@gmail.com>
2021-02-19 14:00:53 +0530

    Add a Console Message for debugging

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const logger = new Logger('Main');
+
+const config = {
+  host: '127.0.0.1',
+  port: 3000,
+};
+
 async function booststrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(config.port, () => {
+    logger.debug(
+      `Nest application is listening on ${config.host}:${config.port}`,
+    );
+  });
 }
 booststrap();
